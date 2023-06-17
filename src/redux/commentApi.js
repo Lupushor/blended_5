26f681d1a0fe9725026e3538c730f06f1e31f260
Aleksandr Lupu <103314959+Lupushor@users.noreply.github.com>
2023-06-17 18:35:58 +0200

    first

--- src/redux/commentApi.js
+++ src/redux/commentApi.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const API_ENDPOINT = '/comments';
-const BASE_URL = '';
+const BASE_URL = 'https://64883ed00e2469c038fd53a7.mockapi.io';
 
 export const commentApi = createApi({
   reducerPath: 'comments',
@@ -9,7 +9,29 @@ export const commentApi = createApi({
     baseUrl: BASE_URL,
   }),
   tagTypes: ['Comments'],
-  endpoints: (builder) => ({}),
+  endpoints: (builder) => ({
+    getComments: builder.guery({
+      query: () => API_ENDPOINT,
+      providesTags: ['Comments'],
+    })
+  }),
+    addComment: builder.mutation({
+    query: (comment) => ({
+      url: API_ENDPOINT,
+      method: 'POST',
+      body: comment
+    }),
+    ivalidatesTags: ['Comments']
+  }),
+    updateCommentCount: builder.mutation({
+    query: ({id, ...body}) => ({
+      url: `${API_ENDPOINT}/${id}`,
+method: 'PUT',
+body
+    }),
+    ivalidatesTags: ['Comments']
+  })
 });
 
-export const {} = commentApi;
+
+export const {useGetCommentsQuery, useAddCommentMutation, useUpdateCommentCountMutation} = commentApi;
